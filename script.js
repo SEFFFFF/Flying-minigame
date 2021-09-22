3a2ccbc3060413313af22782349e9b00aac345c4
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-09-22 20:13:48 +0200

    V0.2 added movement script

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-document.addEventListener("DOMContentLoaded",()=>{
 
-    const ship=document.querySelector(".ship");
-
+let square = document.querySelector('.ship');
+let moveBy = 5;
+window.addEventListener('load', () => {
+    square.style.position = 'absolute';
+    square.style.top = 0;
+    square.style.left = 0;
+  
+});
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'ArrowUp':
+            square.style.top = parseInt(square.style.top) - moveBy + 'px'
+            break;
+        case 'ArrowDown':
+            square.style.top = parseInt(square.style.top) + moveBy + 'px'
+            break;
+        case 'ArrowLeft':
+            square.style.left = parseInt(square.style.left) - moveBy + 'px'
+            break;
+        case 'ArrowRight':
+            square.style.left = parseInt(square.style.left) + moveBy + 'px'
+            break;
+      
+    }
 });
