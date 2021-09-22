7196d66c80016a6fb70a31020d7b45c2054397ed
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-09-22 21:49:45 +0200

    V1.0 + rotation on move, stylized

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const ship = document.querySelector('.ship');
-const move = 5;
+const move = 5; // NUMBER OF SPACES THE SHIP MOVES
 
 // SETS POSITION OF THE SPACE SHIP AND ALLOWS IT TO MOVE
 window.addEventListener('load', () => {
     ship.style.position = 'absolute';
-    ship.style.top = 0;
-    ship.style.left = 0;
+    ship.style.top = 50 + '%';
+    ship.style.left = 40 + '%';
 });
 
 // SETS IMPUTS TO MOVE AROUND SHIP
@@ -13,15 +13,19 @@ window.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'w':
             ship.style.top = parseInt(ship.style.top) - move + 'px' // UP
+            ship.style.transform = 'rotate(0deg)';
             break;
         case 's':
             ship.style.top = parseInt(ship.style.top) + move + 'px'  // DOWN
+            ship.style.transform = 'rotate(180deg)';
             break;
         case 'a':
             ship.style.left = parseInt(ship.style.left) - move + 'px' // LEFT
+            ship.style.transform = 'rotate(270deg)';
             break;
         case 'd':
             ship.style.left = parseInt(ship.style.left) + move + 'px' // RIGHT
+            ship.style.transform = 'rotate(90deg)';
             break;
     }
 });
